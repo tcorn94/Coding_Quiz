0e14a43e5992c613f59532fd44b7cf29dbc45060
Princess Itaya <taycornelius@att.net>
2019-12-05 20:52:23 -0500

    added questions and quiz loop

--- script.js
+++ script.js
@@ -1,24 +1,81 @@
+
+
+
+//create timer function
+
+var timeEl = document.querySelector(".timer");
+var screen = document.querySelector(".container");
+
+
+var secondsLeft = 80;
+
+function setTime() {
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft + " seconds remaining";
+
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      sendMessage();////change send message to end game
+    }
+
+  },1000);// 1000milisecond interval between secondsleft--
+}
+//change send message to end game
+function sendMessage() {
+  timeEl.textContent = " ";
+
+  var imgEl = document.createElement("img");
+
+  imgEl.setAttribute("src", "images/image_1.jpg");
+  mainEl.appendChild(imgEl); //append end game
+
+}
+
+setTime();
+
+///beginning of quiz formatting
+
 var quiz = [
-{"q: Sugar gliders are members of the marsupial class, "a: true"}
 
-{"q:Edgar Allan Poe did NOT write which of the following: ", "a: 'Because I could not stop for Death'"}
+  {q: "Sugar gliders are members of the marsupial class", a: true},
 
-{"q:The dog Blue from Blues Clues was:", "a: female"}
+ {q: "Edgar Allan Poe did NOT write which of the following:", a: "because I could not stop for death"},
+ 
+ {q: "The dog Blue from Blues Clues was", a: "female"},
 
-{"q:Which of the following was the capital of Japan prior to Tokyo?", "a: Kyoto"}
+ {q: "Which of the following was the capital of Japan prior to Tokyo?", a: "Kyoto"},
+ 
+ {q: "Which U.S. President threw up on the Japanese prime minister?", a: "George H. W. Bush"},
 
-{"q:Which U.S. President threw up on the Japanese prime minister? ", "a: George H. W. Bush"}
+ {q: "Black ants can live for up to 15 years", a: true},
+ 
+ {q: "As of 2017, which US state's main export was Zinc?", a: "Alaska"},
 
-{"q:Black ants can live for up to 15 years", "a: true"}
-
-{"q: As of 2017, which US state's main export was Zinc?", "a: Alaska"}
-
-{"q:What is Lady Gaga's real first name?", "a: Stefani"}
-
-{"q:During which year was the film Titanic released?", "a: 1997"}
-
-{"q: During mating, the female praying mantis does what?", "a: Bites off the male's head."}
+ {q: "During which year was the film Titanic released?:", a: "1997"},
 
 
 
 ]
+
+var score = 0 
+
+for (var i = 0; i < quiz.length; i++) {
+
+ var answer = confirm(quiz[i].q);
+
+ if (answer === quiz[i].a) {
+
+     alert("You are correct!");
+     score ++;
+
+  
+ }
+ else {
+         alert("Sorry, try again!");
+     }
+}
+
+
+
+alert("you got" + score + "/" + quiz.length)
